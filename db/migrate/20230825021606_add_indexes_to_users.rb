b3ae9f4eba35d1201914e69be281987bfc8c694f
Тимур Шахлай <timur@MacBook-Air-Timur.local>
2023-08-25 19:38:28 +0300

    Added User model and seeds

---
+++ db/migrate/20230825021606_add_indexes_to_users.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddIndexesToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
